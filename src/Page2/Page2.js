7322af916082448c24f1e83a06ae8bbf5fb28c97
mehdi22256 <mehdi22256@gmail.com>
2024-06-29 16:37:52 +0300

    backup

--- src/Page2/Page2.js
+++ src/Page2/Page2.js
@@ -5,6 +5,15 @@ import bootStrapLogo from "./pics2/bootstrap_5968672.png";
 import figmaLogo from "./pics2/figma_5968705.png";
 import tailWindLogo from "./pics2/tailwind-css.svg";
 import reactLogo from "./pics2/physics_1126012.png";
+import pythonLogo from "./pics2/python.png";
+import cLogo from "./pics2/C++.png";
+import canvaLogo from "./pics2/canva-icon.png";
+import fireLogo from "./pics2/firebase.png";
+import reduxLogo from "./pics2/redux.png";
+import expressLogo from "./pics2/express.png";
+import mongoLogo from "./pics2/mongodb.png";
+import nodeLogo from "./pics2/node.png";
+import socketLogo from "./pics2/socket_io.png";
 import Aos from "aos";
 import "aos/dist/aos.css";
 const Page2 = ({ darkMode }) => {
@@ -19,16 +28,109 @@ const Page2 = ({ darkMode }) => {
       id="about"
       className={`${darkMode && "dark"} mx-auto font-nunito`}
     >
-      <div className=" bg-primary2 dark:bg-darkPrimary2 h-auto md:h-auto pb-20 md:pb-52 pt-10">
-        <div className="text-center pt-5 md:text-left md:pt-32 md:pl-36 md:mb-auto">
+      <div className="flex flex-row flex-wrap gap-5 justify-center bg-primary2 dark:bg-darkPrimary2 h-auto md:h-auto pb-20">
+        <div className="text-center m-auto md:text-left pt-16 md:pt-1 md:pl-36 md:mb-auto">
           <p className="font-extrabold text-6xl md:text-7xl text-fontPrimary dark:text-fontDarkPrimary">
             About me.
           </p>
         </div>
-        <div className="flex flex-row-reverse justify-center items-center content-center flex-wrap space-14 gap-10 mt-24">
+        <div
+          data-aos="fade-down"
+          className="w-80 md:w-[700px] 2xl:mt-20 h-auto border-2 mt-5 border-fontPrimary bg-fontDarkPrimary rounded-3xl dark:bg-darkPrimary2 dark:border-fontDarkPrimary m-auto 2xl:top-36 2xl:left-48"
+        >
+          <div className="flex flex-row border-b-2 pb-2 border-fontPrimary dark:border-fontDarkPrimary">
+            <div className="w-5 h-5 rounded-full bg-fontPrimary m-1 mt-4 ml-3 dark:bg-fontDarkPrimary"></div>
+            <div className="w-5 h-5 rounded-full bg-fontPrimary m-1 mt-4 dark:bg-fontDarkPrimary"></div>
+            <div className="w-5 h-5 rounded-full bg-fontPrimary m-1 mt-4 dark:bg-fontDarkPrimary"></div>
+            <div className="self-end ml-28 md:ml-40">
+              <p className=" font-extrabold w-max text-xl text-fontPrimary dark:text-fontDarkPrimary">
+                My Skills
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-center flex-wrap gap-10 content-center p-5">
+            <img
+              className="w-14 h-14 md:w-20 md:h-20"
+              src={htmlLogo}
+              alt="html"
+            />
+            <img
+              className="w-14 h-14 md:w-20 md:h-20"
+              src={cssLogo}
+              alt="css"
+            />
+            <img
+              className="w-14 h-14 md:w-20 md:h-20 rounded-lg"
+              src={jsLogo}
+              alt="java script"
+            />
+            <img
+              className="w-14 h-14 md:w-20 md:h-20 dark:brightness-200"
+              src={bootStrapLogo}
+              alt="bootstrap"
+            />
+            <img
+              className="w-14 h-14 md:w-20 md:h-20"
+              src={reactLogo}
+              alt="react"
+            />
+            <img
+              className="w-14 h-14 md:w-20 md:h-20"
+              src={tailWindLogo}
+              alt="tailwind"
+            />
+            <img
+              className="w-14 h-14 md:w-20 md:h-20"
+              src={figmaLogo}
+              alt="figma"
+            />
+            <img
+              className="w-14 h-14 md:w-20 md:h-20"
+              src={reduxLogo}
+              alt="redux"
+            />
+            <img
+              className="w-14 h-14 md:w-20 md:h-20"
+              src={socketLogo}
+              alt="socket.io"
+            />
+            <img
+              className="w-14 h-14 md:w-20 md:h-20"
+              src={canvaLogo}
+              alt="canva"
+            />
+            <img className="w-14 h-14 md:w-20 md:h-20" src={cLogo} alt="C++" />
+            <img
+              className="w-14 h-14 md:w-20 md:h-20"
+              src={pythonLogo}
+              alt="python"
+            />
+            <img
+              className="w-14 h-14 md:w-20 md:h-20"
+              src={fireLogo}
+              alt="fire base"
+            />
+            <img
+              className="w-14 h-14 md:w-20 md:h-20"
+              src={expressLogo}
+              alt="express js"
+            />
+            <img
+              className="w-14 h-14 md:w-20 md:h-20"
+              src={nodeLogo}
+              alt="node js"
+            />
+            <img
+              className="w-14 h-14 md:w-20 md:h-20"
+              src={mongoLogo}
+              alt="mongo db"
+            />
+          </div>
+        </div>
+        <div className="flex flex-row-reverse justify-center items-center content-center flex-wrap space-14 gap-5">
           <div
             data-aos="fade-down"
-            className="w-80 md:w-96 2xl:w-1/3 h-auto pb-5 border-2 border-fontPrimary bg-fontDarkPrimary rounded-3xl dark:bg-darkPrimary2 dark:border-fontDarkPrimary 2xl:relative 2xl:bottom-64 2xl:right-40"
+            className="w-80 md:w-96 2xl:w-1/3 h-auto pb-5 border-2 border-fontPrimary bg-fontDarkPrimary rounded-3xl dark:bg-darkPrimary2 dark:border-fontDarkPrimary 2xl:bottom-64 2xl:right-40"
           >
             <div className="flex flex-row border-b-2 pb-2 border-fontPrimary dark:border-fontDarkPrimary">
               <div className="w-5 h-5 rounded-full bg-fontPrimary m-1 mt-4 ml-3 dark:bg-fontDarkPrimary"></div>
@@ -47,61 +149,10 @@ const Page2 = ({ darkMode }) => {
               </p>
             </div>
           </div>
-          <div
-            data-aos="fade-down"
-            className="w-80 md:w-96 h-auto border-2 border-fontPrimary bg-fontDarkPrimary rounded-3xl dark:bg-darkPrimary2 dark:border-fontDarkPrimary 2xl:relative 2xl:top-36 2xl:left-48"
-          >
-            <div className="flex flex-row border-b-2 pb-2 border-fontPrimary dark:border-fontDarkPrimary">
-              <div className="w-5 h-5 rounded-full bg-fontPrimary m-1 mt-4 ml-3 dark:bg-fontDarkPrimary"></div>
-              <div className="w-5 h-5 rounded-full bg-fontPrimary m-1 mt-4 dark:bg-fontDarkPrimary"></div>
-              <div className="w-5 h-5 rounded-full bg-fontPrimary m-1 mt-4 dark:bg-fontDarkPrimary"></div>
-              <div className="self-end ml-28 md:ml-40">
-                <p className=" font-extrabold w-max text-xl text-fontPrimary dark:text-fontDarkPrimary">
-                  My Skills
-                </p>
-              </div>
-            </div>
-            <div className="flex justify-center flex-wrap gap-10 content-center p-5">
-              <img
-                className="w-14 h-14 md:w-20 md:h-20"
-                src={htmlLogo}
-                alt="html"
-              />
-              <img
-                className="w-14 h-14 md:w-20 md:h-20"
-                src={cssLogo}
-                alt="css"
-              />
-              <img
-                className="w-14 h-14 md:w-20 md:h-20 rounded-lg"
-                src={jsLogo}
-                alt="java script"
-              />
-              <img
-                className="w-14 h-14 md:w-20 md:h-20 dark:brightness-200"
-                src={bootStrapLogo}
-                alt="bootstrap"
-              />
-              <img
-                className="w-14 h-14 md:w-20 md:h-20"
-                src={reactLogo}
-                alt="react"
-              />
-              <img
-                className="w-14 h-14 md:w-20 md:h-20"
-                src={tailWindLogo}
-                alt="tailwind"
-              />
-              <img
-                className="w-14 h-14 md:w-20 md:h-20"
-                src={figmaLogo}
-                alt="figma"
-              />
-            </div>
-          </div>
+
           <div
             data-aos="fade-up-right"
-            className="w-80 md:w-96 2xl:w-1/3 h-auto pb-5 border-2 border-fontPrimary bg-fontDarkPrimary rounded-3xl md:mb-auto 2xl:relative 2xl:left-10 dark:bg-darkPrimary2 dark:border-fontDarkPrimary "
+            className="w-80 md:w-96 2xl:w-1/3 h-auto pb-5 border-2 border-fontPrimary bg-fontDarkPrimary rounded-3xl md:mb-auto 2xl:relative 2xl:right-32 bottom-40 dark:bg-darkPrimary2 dark:border-fontDarkPrimary"
           >
             <div className="flex flex-row border-b-2 pb-2 border-fontPrimary dark:border-fontDarkPrimary">
               <div className="w-5 h-5 rounded-full bg-fontPrimary m-1 mt-4 ml-3 dark:bg-fontDarkPrimary"></div>
--- src/Page3/Page3.js
+++ src/Page3/Page3.js
@@ -5,8 +5,8 @@ import arrow1 from "./pic3/Vector 1.png";
 import arrow2 from "./pic3/Vector 2.png";
 import lightArrow1 from "./pic3/lightVector 1.png";
 import lightArrow2 from "./pic3/lightVector 2.png";
-import space from "./pic3/space.png";
-import bigByte from "./pic3/bigByte.png";
+import blogs from "./pic3/blogs.png";
+import mint from "./pic3/mint.png";
 import Aos from "aos";
 import "aos/dist/aos.css";
 const Page3 = ({ darkMode }) => {
@@ -21,17 +21,90 @@ const Page3 = ({ darkMode }) => {
       id="works"
       className={`${darkMode && "dark"} mx-auto font-nunito`}
     >
-      <div className=" h-auto px-3 pb-40 bg-primary3 text-fontPrimary dark:text-fontDarkPrimary dark:bg-darkPrimary3">
+      <div className="h-[3250px] md:h-[3100px] xl:h-[3100px] px-3 md:px-40 lg:px-3 pb-40 bg-primary3 text-fontPrimary dark:text-fontDarkPrimary dark:bg-darkPrimary3 ">
         <div className="text-center lg:text-right pt-16 lg:pt-32 lg:pr-72">
-          <p className=" font-extrabold text-6xl md:text-7xl text-fontPrimary dark:text-fontDarkPrimary">
+          <p className="font-extrabold text-6xl md:text-7xl text-fontPrimary dark:text-fontDarkPrimary">
             My Works.
           </p>
         </div>
-
-        {/* first */}
         <div className="flex flex-col flex-wrap">
-          <div className="flex flex-col lg:flex-row mt-20 lg:px-32 lg:gap-14 relative">
-            <div className="absolute left-52 bottom-96 mb-12 mr-20">
+          {/* first */}
+          <div className="flex flex-col lg:flex-row-reverse mt-20 lg:px-32 lg:gap-5 relative">
+            <div
+              data-aos="fade-down"
+              className="w-auto lg:mx-auto xl:w-1/2 xl:mt-10"
+            >
+              {/* <a href="##" target="_blank" rel="noreferrer"> */}
+              <p
+                className="hover:bg-primary dark:hover:bg-darkPrimary hover:rounded-full cursor-pointer rounded-full
+              pl-6 lg:pl-0 pb-8 md:pb-0 w-max lg:text-center xl:m-5 xl:mt-12 font-bold text-4xl lg:text-6xl lg:mb-20 text-fontPrimary dark:text-fontDarkPrimary"
+              >
+                Mint
+              </p>
+              {/* </a> */}
+              <p className="mx-5 lg:ml-5 line text-2xl md:text-2xl font-mono text-fontPrimary dark:text-fontDarkPrimary text-justify">
+                it’s a medical project, you can register on the website and
+                contact with with your doctor or pharmacist and booking an
+                appointment with a doctor, you also can publish medical article
+                if you were a doctor or pharmacist.
+              </p>
+            </div>
+            <div
+              data-aos="zoom-out"
+              className="w-72 md:w-96 lg:w-1/2 mx-auto z-10"
+            >
+              <div className="flex flex-row gap-5">
+                <a
+                  href="https://github.com/mehdi22256/mint-frontend"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="hover:-translate-x-3 hover:translate-y-3 hover:shadow-none  text-center w-24 md:w-32 lg:w-56 md:h-16 bg-lightNav relative top-5 left-16 shadow-reverselight cursor-pointer">
+                    <p className="text-center py-1 md:pt-4 font-semibold text-lg xl:text-2xl font-serif text-fontPrimary ">
+                      Front End
+                    </p>
+                  </div>
+                </a>
+                <a
+                  href="https://github.com/mehdi22256/mint"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="hover:-translate-x-3 hover:translate-y-3 hover:shadow-none  text-center w-24 md:w-32 lg:w-56 md:h-16 bg-lightNav relative top-5 left-16 shadow-reverselight cursor-pointer">
+                    <p className="text-center py-1 md:pt-4 font-semibold text-lg xl:text-2xl font-serif text-fontPrimary ">
+                      Back End
+                    </p>
+                  </div>
+                </a>
+              </div>
+              <div>
+                <img
+                  className="shadow-reverselight dark:shadow-reversedark"
+                  src={mint}
+                  alt="mint"
+                />
+              </div>
+            </div>
+            <div className="absolute w-1/2 top-32 right-10 ">
+              {darkMode && (
+                <img
+                  className="invisible 2xl:visible "
+                  src={lightArrow2}
+                  alt="arrow"
+                />
+              )}
+              <img
+                className="invisible 2xl:visible dark:hidden"
+                src={arrow2}
+                alt="arrow"
+              />
+            </div>
+          </div>
+
+          {/* second */}
+
+          <div className="flex flex-col lg:flex-row mt-20 xl:mt-80 lg:px-32 lg:gap-5 relative">
+            <div className="absolute left-52 bottom-96 mb-14 mr-20">
               {darkMode && (
                 <img
                   className="invisible 2xl:visible"
@@ -45,52 +118,67 @@ const Page3 = ({ darkMode }) => {
                 alt="arrow"
               />
             </div>
-            <div data-aos="zoom-in-right" className=" w-auto mx-auto xl:w-1/2">
-              <a
-                href="https://bakingshop.netlify.app/"
-                target="_blank"
-                rel="noreferrer"
+            <div
+              data-aos="fade-down"
+              data-aos-duration="500"
+              className="w-auto mx-auto xl:w-1/2"
+            >
+              {/* <a href="##" target="_blank" rel="noreferrer"> */}
+              <p
+                className="hover:bg-primary dark:hover:bg-darkPrimary hover:rounded-full cursor-pointer rounded-full
+              pl-6 lg:pl-0 w-max text-center xl:m-5 xl:mt-12 font-bold text-4xl lg:text-6xl lg:mb-20 text-fontPrimary dark:text-fontDarkPrimary"
               >
-                <p
-                  className="hover:bg-primary dark:hover:bg-darkPrimary hover:rounded-full cursor-pointer rounded-full 
-              pl-6 md:pl-0 w-max text-center font-bold text-4xl lg:text-6xl pb-8 lg:pb-0 lg:mb-16 xl:mt-16 text-fontPrimary dark:text-fontDarkPrimary"
-                >
-                  Bakery Shop
-                </p>
-              </a>
-              <p className="line md:mr-4 pl-4 lg:pl-0 text-2xl leading-loose font-mono md:text-4xl text-fontPrimary dark:text-fontDarkPrimary">
-                It’s a website about a bakery shop, containing what trending
-                items and services they can offer for their customers.{" "}
+                Wiki Blogs
+              </p>
+              {/* </a> */}
+              <p className="line m-5 text-2xl md:text-2xl font-mono text-fontPrimary dark:text-fontDarkPrimary text-justify">
+                You can explore the blogs you want to read on wiki, and if you
+                want to create your own blog, you also can do, just register on
+                wiki blogs and share your blog
               </p>
             </div>
             <div
-              data-aos="zoom-in"
+              data-aos="flip-down"
+              data-aos-duration="500"
               className="w-72 md:w-96 lg:w-1/2 mx-auto z-10"
             >
-              <a
-                href="https://bakingshop.netlify.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="hover:translate-x-3 hover:translate-y-3 hover:shadow-none w-40 md:w-48 md:h-16 bg-lightNav relative top-5 left-16 shadow-3xl cursor-pointer">
-                  <p className=" text-center py-1 md:pt-4 font-semibold text-2xl font-serif text-fontPrimary ">
-                    Bakery Shop
-                  </p>
-                </div>
-              </a>
+              <div className="flex flex-row gap-5">
+                <a
+                  href="https://github.com/mehdi22256/blogs_frontEnd"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="hover:translate-x-3 hover:translate-y-3 hover:shadow-none w-24 md:w-32 lg:w-56 md:h-16 bg-lightNav relative top-5 left-16 shadow-3xl cursor-pointer">
+                    <p className="text-center py-1 md:pt-4 font-semibold text-lg xl:text-2xl font-serif text-fontPrimary ">
+                      Front End
+                    </p>
+                  </div>
+                </a>
+                <a
+                  href="https://github.com/mehdi22256/blogs_backEnd"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="hover:translate-x-3 hover:translate-y-3 hover:shadow-none w-24 md:w-32 lg:w-56 md:h-16 bg-lightNav relative top-5 left-16 shadow-3xl cursor-pointer">
+                    <p className="text-center py-1 md:pt-4 font-semibold text-lg xl:text-2xl font-serif text-fontPrimary ">
+                      Back End
+                    </p>
+                  </div>
+                </a>
+              </div>
               <div>
                 <img
                   className="shadow-3xl dark:shadow-darkshadow"
-                  src={bakery}
-                  alt="bakery"
+                  src={blogs}
+                  alt="blogs"
                 />
               </div>
             </div>
           </div>
 
-          {/* second */}
+          {/* third */}
 
-          <div className="flex flex-col lg:flex-row-reverse mt-20 lg:px-32 lg:gap-14 relative">
+          <div className="flex flex-col lg:flex-row-reverse mt-20 lg:px-32 lg:gap-5 relative">
             <div data-aos="fade-down" className="w-auto mx-auto xl:w-1/2">
               <a
                 href="https://pokemonscards.netlify.app/"
@@ -104,7 +192,7 @@ const Page3 = ({ darkMode }) => {
                   Pokémon Cards
                 </p>
               </a>
-              <p className="line m-5 2xl:mx-16 text-2xl md:text-4xl leading-relaxed font-mono text-fontPrimary dark:text-fontDarkPrimary">
+              <p className="line m-5 2xl:mx-16 text-2xl md:text-2xl leading-relaxed font-mono text-fontPrimary dark:text-fontDarkPrimary text-justify">
                 If you love Pokémon, you will this website, because it contains
                 the informations about the Pokémons
               </p>
@@ -132,7 +220,7 @@ const Page3 = ({ darkMode }) => {
                 />
               </div>
             </div>
-            <div className="absolute w-1/2 top-36 ">
+            <div className="absolute w-1/2 top-32 right-10 ">
               {darkMode && (
                 <img
                   className="invisible 2xl:visible "
@@ -148,10 +236,9 @@ const Page3 = ({ darkMode }) => {
             </div>
           </div>
 
-          {/* third */}
-
-          <div className="flex flex-col lg:flex-row mt-20 xl:mt-80 lg:px-32 lg:gap-14 relative">
-            <div className="absolute left-52 bottom-96 mb-14 mr-20">
+          {/* fourth */}
+          <div className="flex flex-col lg:flex-row mt-20 xl:mt-80 lg:px-32 lg:gap-5 relative">
+            <div className="absolute left-52 bottom-96 mb-12 mr-20">
               {darkMode && (
                 <img
                   className="invisible 2xl:visible"
@@ -165,113 +252,46 @@ const Page3 = ({ darkMode }) => {
                 alt="arrow"
               />
             </div>
-            <div
-              data-aos="flip-up"
-              data-aos-duration="500"
-              className="w-auto mx-auto xl:w-1/2"
-            >
+            <div data-aos="zoom-in-right" className=" w-auto mx-auto xl:w-1/2">
               <a
-                href="https://ourspaces.netlify.app/"
+                href="https://bakingshop.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
               >
                 <p
-                  className="hover:bg-primary dark:hover:bg-darkPrimary hover:rounded-full cursor-pointer rounded-full
-              pl-6 lg:pl-0 w-max text-center xl:m-5 xl:mt-12 font-bold text-4xl lg:text-6xl lg:mb-20 text-fontPrimary dark:text-fontDarkPrimary"
+                  className="hover:bg-primary dark:hover:bg-darkPrimary hover:rounded-full cursor-pointer rounded-full 
+              pl-6 md:pl-0 w-max text-center font-bold text-4xl lg:text-6xl pb-8 lg:pb-0 lg:mb-16 xl:mt-16 text-fontPrimary dark:text-fontDarkPrimary"
                 >
-                  Our Space
+                  Bakery Shop
                 </p>
               </a>
-              <p className="line m-5 text-2xl md:text-4xl font-mono text-fontPrimary dark:text-fontDarkPrimary">
-                You can explore the galaxy with beautiful pictures for the
-                planets of our universe.
+              <p className="line mx-5 md:mr-4 lg:pl-0 text-2xl leading-loose font-mono md:text-2xl text-fontPrimary dark:text-fontDarkPrimary text-justify">
+                It’s a website about a bakery shop, containing what trending
+                items and services they can offer for their customers.{" "}
               </p>
             </div>
             <div
-              data-aos="flip-down"
-              data-aos-duration="500"
+              data-aos="zoom-in"
               className="w-72 md:w-96 lg:w-1/2 mx-auto z-10"
             >
               <a
-                href="https://ourspaces.netlify.app/"
+                href="https://bakingshop.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
               >
                 <div className="hover:translate-x-3 hover:translate-y-3 hover:shadow-none w-40 md:w-48 md:h-16 bg-lightNav relative top-5 left-16 shadow-3xl cursor-pointer">
-                  <p className="text-center py-1 md:pt-4 font-semibold text-2xl font-serif text-fontPrimary ">
-                    Our Space
+                  <p className=" text-center py-1 md:pt-4 font-semibold text-2xl font-serif text-fontPrimary ">
+                    Bakery Shop
                   </p>
                 </div>
               </a>
               <div>
                 <img
-                  className="shadow-3xl dark:shadow-darkshadow"
-                  src={space}
-                  alt="space"
+                  className="shadow-3xl dark:shadow-darkshadow z-50"
+                  src={bakery}
+                  alt="bakery"
                 />
               </div>
-            </div>
-          </div>
-
-          {/* fourth */}
-
-          <div className="flex flex-col lg:flex-row-reverse mt-20 lg:px-32 lg:gap-14 relative">
-            <div
-              data-aos="fade-down"
-              className="w-auto lg:mx-auto xl:w-1/2 xl:mt-10"
-            >
-              <a
-                href="https://deliciousbyte.netlify.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <p
-                  className="hover:bg-primary dark:hover:bg-darkPrimary hover:rounded-full cursor-pointer rounded-full
-              pl-6 lg:pl-0 pb-8 md:pb-0 w-max lg:text-center xl:m-5 xl:mt-12 font-bold text-4xl lg:text-6xl lg:mb-20 text-fontPrimary dark:text-fontDarkPrimary"
-                >
-                  Delicious Byte
-                </p>
-              </a>
-              <p className="ml-5 line text-2xl md:text-4xl font-mono text-fontPrimary dark:text-fontDarkPrimary">
-                it’s about fast food with a collection of types you can order.
-              </p>
-            </div>
-            <div
-              data-aos="zoom-out"
-              className="w-72 md:w-96 lg:w-1/2 mx-auto z-10"
-            >
-              <a
-                href="https://deliciousbyte.netlify.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <div className="hover:-translate-x-3 hover:translate-y-3 hover:shadow-none  text-center w-44 md:w-56 md:h-16 bg-lightNav relative top-5 left-16 shadow-reverselight cursor-pointer">
-                  <p className="text-center py-1 md:pt-4 font-semibold text-2xl font-serif text-fontPrimary ">
-                    Delicious Byte
-                  </p>
-                </div>
-              </a>
-              <div>
-                <img
-                  className="shadow-reverselight dark:shadow-reversedark"
-                  src={bigByte}
-                  alt="byte"
-                />
-              </div>
-            </div>
-            <div className="absolute w-1/2 top-36 ">
-              {darkMode && (
-                <img
-                  className="invisible 2xl:visible "
-                  src={lightArrow2}
-                  alt="arrow"
-                />
-              )}
-              <img
-                className="invisible 2xl:visible dark:hidden"
-                src={arrow2}
-                alt="arrow"
-              />
             </div>
           </div>
         </div>
